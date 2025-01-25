7967154f6429df57d023e2b3f809ab99fa68dd9a
skylineagle <barmghr@gmail.com>
2025-01-25 15:59:40 +0200

    fix: support pocketbase for baker and app

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -2,5 +2,5 @@ import PocketBase from "pocketbase";
 import { TypedPocketBase } from "@/types/db.types";
 
 export const pb = new PocketBase(
-  Bun.env.POCKETBASE_URL || "http://127.0.0.1:8090"
+  process.env.POCKETBASE_URL || "http://127.0.0.1:8090"
 ) as TypedPocketBase;
