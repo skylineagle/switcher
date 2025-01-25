2713eac70b8c34cfddc23bd8fa3c8a7c833407c3
skylineagle <barmghr@gmail.com>
2025-01-25 16:34:42 +0200

    fix: set pocketbase to use env vaes

--- pocketbase/pb_hooks/job.utils.js
+++ pocketbase/pb_hooks/job.utils.js
@@ -1,12 +1,14 @@
 /// <reference path="../pb_data/types.d.ts" />
 
+const BAKER_URL = $os.getenv("BAKER_URL") || "http://host.docker.internal:3000";
+
 function createCameraJob(cameraId, automation) {
   $http.send({
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    url: `http://host.docker.internal:3000/jobs/${cameraId}`,
+    url: `${BAKER_URL}/jobs/${cameraId}`,
     body: JSON.stringify(automation),
   });
 }
@@ -14,7 +16,7 @@ function createCameraJob(cameraId, automation) {
 function deleteCameraJob(cameraId) {
   const response = $http.send({
     method: "DELETE",
-    url: `http://host.docker.internal:3000/jobs/${cameraId}`,
+    url: `${BAKER_URL}/jobs/${cameraId}`,
   });
   $app.logger().info(response.statusCode);
 }
@@ -22,21 +24,21 @@ function deleteCameraJob(cameraId) {
 function startCameraJob(cameraId) {
   $http.send({
     method: "POST",
-    url: `http://host.docker.internal:3000/jobs/${cameraId}/start`,
+    url: `${BAKER_URL}/jobs/${cameraId}/start`,
   });
 }
 
 function stopCameraJob(cameraId) {
   $http.send({
     method: "POST",
-    url: `http://host.docker.internal:3000/jobs/${cameraId}/stop`,
+    url: `${BAKER_URL}/jobs/${cameraId}/stop`,
   });
 }
 
 function getJobStatus(cameraId) {
   const response = $http.send({
     method: "GET",
-    url: `http://host.docker.internal:3000/jobs/${cameraId}`,
+    url: `${BAKER_URL}/jobs/${cameraId}`,
   });
   return response.json.status;
 }
--- pocketbase/pb_hooks/mediamtx.utils.js
+++ pocketbase/pb_hooks/mediamtx.utils.js
@@ -1,12 +1,14 @@
 /// <reference path="../pb_data/types.d.ts" />
 
+const MEDIAMTX_API = $os.getenv("MEDIAMTX_API") || "http://mediamtx:9997";
+
 function addMediaMTXPath(name, configuration) {
   $http.send({
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    url: `http://mediamtx:9997/v3/config/paths/add/${name}`,
+    url: `${MEDIAMTX_API}/v3/config/paths/add/${name}`,
     body: JSON.stringify({ name, ...configuration }),
   });
 }
@@ -14,7 +16,7 @@ function addMediaMTXPath(name, configuration) {
 function deleteMediaMTXPath(name) {
   const response = $http.send({
     method: "DELETE",
-    url: `http://mediamtx:9997/v3/config/paths/delete/${name}`,
+    url: `${MEDIAMTX_API}/v3/config/paths/delete/${name}`,
   });
   $app.logger().info(response.statusCode);
 }
